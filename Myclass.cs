aa8e2ca8b440bed986e5a4e47cac7d448a5d8879
Shihab-hossain-shuvo <shuvokhanrs2015@gmail.com>
2020-12-21 00:28:39 +0600

    initial commit

---
+++ Myclass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentDiary
+{
+    class Myclass
+    {
+        public string Name { get; set; }
+
+        public string Date { get; set; }
+        public string Pictures { get; set; }
+
+        public string Username { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
